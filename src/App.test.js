e1a6b8d61e92bb9c1bc02bc7f4630627acef69b9
pratik kakkad <pratik.kakkad.neosoftmail.com>
2022-12-20 17:56:38 +0530

    update test title

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 const MockApp = () => {
   return (
-      <BrowserRouter>
-        <App/>
-      </BrowserRouter>
-  )
-}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+};
 
-test('renders learn react link', () => {
+test("should renders breadcrumb", () => {
   render(<MockApp />);
   const linkElement = screen.getByText(/Logger search/i);
   expect(linkElement).toBeInTheDocument();
